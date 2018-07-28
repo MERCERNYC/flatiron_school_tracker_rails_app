482123acc756353512c25c5a9724693d931ac4fe
github username <github email address>
2018-07-27 22:36:33 -0400

    added subject create action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,26 +1,13 @@
 class SubjetcsController < ApplicationController
 
-  def new
-    @subject = Subject.new
-    @subject.topics.build
-   end
-  end
-
   def create
-    @subject = Subject.new(subject_params)
+    @topic = Topic.find(params[:topic_id])
+    @subject = @topic.subjects.build(subject_params)
     if @subject.save
-      flash[:success] = "Subject was sucessfully created!"
       redirect_to topic_path(@topic)
     else
-      flash[:success] = "Unable to create subject!"
-      redirect_to topic_path(@topic)
+      render "topics/show"
     end
-  end
-
-  def destroy
-    @subject.destroy
-    flash[:success] = "Subject Deleted!"
-    redirect_to topics_path(@topic)
   end
 
   private
