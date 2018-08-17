15701d278133a413348e40c9d2ff801a33ccaa42
github username <github email address>
2018-08-17 15:50:04 -0400

    added show method to subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,15 +5,17 @@ class SubjectsController < ApplicationController
     @subject = Subject.new
   end
 
+
   def show
-    @subject = Subject.find(params[:id])
+    # @subject = @topic.subjects.find_by(id: params[:id])
+      @subject = Subject.all
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
+    @topic = Topic.find(params[:topic_id])# finding the parent
     @subject = @topic.subjects.build(subject_params)
     if @subject.save
-      redirect_to topics_path
+      redirect_to topic_subject_path(@topic)
       # topics/:topic_id/subjects/:id
     else
       render "topics/show"
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,16 +2,11 @@
     belongs_to :student
     has_many :subjects
 
-    validates :title, :presence => true
-    validates :lab, :presence => true
-
-
     accepts_nested_attributes_for :subjects
     # scope :two_subjects_or_more, -> {)}
 
      def self.two_subjects_or_more
        self.select {|topic| topic.subjects.size > 2 }
      end
-
 
   end
