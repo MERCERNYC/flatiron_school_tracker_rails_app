808d3a79ff0345d816b10d278b17e89438cda5eb
github username <github email address>
2018-08-18 14:09:31 -0400

    fixed assotiations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,6 @@
   #join table
   class Subject < ActiveRecord::Base
     belongs_to :topic #adds methods to the model
+    # belongs_to :student
     delegate :student, to: :topic # it is associated to the student only because it belongs to a topic, which belongs to a student
   end
