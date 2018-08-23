7d2ecbaad548f1812a5af9ead2b23a5423b29379
github username <github email address>
2018-08-23 15:23:32 -0400

    add comments

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,9 @@ class Student < ActiveRecord::Base
   has_many :subjects, through: :topics
   #subjects_ids=ids
 
+  #has_many :subjects
+  #has_many :topics, through: subjects
+
   has_secure_password
 
   validates :name, :presence => true
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,7 @@
   class Subject < ActiveRecord::Base
     belongs_to :topic # foreign key - topic_id
     delegate :student, to: :topic # it is associated to the student only because it belongs to a topic, which belongs to a student
+
+    #belongs_to :topics
+    #belongs_to :student
   end
