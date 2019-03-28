1d02ffd8456ce0213f2b9f7ec20fcf2a9cd2e960
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-03-28 14:00:06 -0400

    add function listTopics with prototype

---
+++ app/assets/javascripts/topics.js
@@ -0,0 +1,47 @@
+// $(function() {
+//  console.log('topics.js loading')
+// });
+
+$(document).ready(() => {
+ alert("yo!")
+ listTopics()
+})
+
+//or use function with no arrow
+// function listTopics (){}
+const listTopics = () => {
+  $('.topics_data').on('click', function(event){
+    event.preventDefault()
+    history.pushState(null, null, 'topics')
+    // getTopics()
+    fetch(`/topics.json`)
+    .then(resp => resp.json())
+    .then(topics => {
+      $('#topic_container').html('')
+      topics.forEach(topic => {
+        let newTopic = new Topic(topic)
+        let topicHtml = newTopic.formatTopic()
+        $('#topic_container').append(topicHtml)
+      })
+    })
+  })
+}
+
+function Topic(topic) {
+  this.id= topic.id
+  this.timeline = topic.timeline
+  this.title = topic.title
+  this.lab = topic.lab
+  this.study_group = topic.study_group
+}
+
+Topic.prototype.formatTopic = function(){
+  let topicHtml = `
+
+  <tr>
+    <td><a href="/topics/${this.timeline}" data-id="${this.id}" </a></td>
+  </tr>
+
+    `
+    return topicHtml
+}
