f398f5728f40774694b55701223d3c1ffbdff8a3
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-03-29 22:47:31 -0400

    add prototypes

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -42,6 +42,40 @@ const postTopicsShow = () => {
   })
 }
 
+//Method on the prototype for JS objects
+
+//INDEX
+Topic.prototype.renderIndex = function() {
+  let topicHtml = `
+
+   <tr>
+      <td><a href="/topics/${this.id}" data-id="${this.id}" class="show_topics"><h1>${this.title}</h1></a></td>
+      <td><button><a href="/topics/${this.id}/edit" data-id="${this.id}" class="update_topic"> Edit</a></button></td>
+      <td><button><a href="/topics/${this.id}" data-id="${this.id}" class="delete_topic" data-method="delete">Delete</a></button></td>
+    </tr>
+    `
+  return topicHtml
+}
+
+//SHOW
+Topic.prototype.renderShow = function() {
+  let topicHtml = `
+
+  <h1>Topic Created</h1>
+
+  <tr>
+   <td><strong>Timeline:</strong> ${this.timeline}</td></br>
+   <td><strong>Title:</strong> ${this.title}</td></br>
+   <td><strong>Lab:</strong> ${this.lab}</td></br>
+   <td><strong>Study Group:</strong> ${this.study_group}</td></br>
+  </tr>
+
+  <button><a href="/topics" class="topic_index">Back to All Topics</a></button>
+
+  `
+  return topicHtml
+}
+
 //Constructor Function to create objects
 function Topic(topic) {
   this.id = topic.id
@@ -50,3 +84,20 @@ function Topic(topic) {
   this.lab = topic.lab
   this.study_group = topic.study_group
 }
+
+//form submited with Ajax
+
+$(function() {
+  $('form#new_topic').on('submit',function (e) {
+    e.preventDefault()
+
+    const values = $(this).serialize()
+    $.post('/topics', values).done(function(data) {
+    $('#topic_container').html(" ")
+
+    const newTopic = new Topic(data)
+    const htmltoAdd = newTopic.renderShow()
+    $('#topic_container').html(htmltoAdd)
+    })
+  })
+})
